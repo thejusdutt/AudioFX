de2155a585c10dd3f61f02287066afe7fc314ce0
Thejus <dutt.thejus@gmail.com>
2020-04-23 11:25:47 +0530

    Basic Code for training without negative value error
    Basic Code for training without negative value error

--- training.py
+++ training.py
@@ -24,18 +24,19 @@ test_output_fft = fft (test_output)
 
 model = keras.Sequential([
 
-    keras.layers.Embedding(1000000, 1000000, input_length=1),
-    keras.layers.Dense(128, activation='linear'),
-    keras.layers.Dense(1)
+    keras.layers.Dense(12,input_dim=1,activation='relu'),
+    keras.layers.Dense(24, activation='relu'),
+    keras.layers.Dense(1,activation='sigmoid')
 ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(train_input_fft, train_output_fft, epochs=25)
+model.fit(train_input_fft, train_output_fft, epochs=5)
 
 test_loss, test_acc = model.evaluate(test_input_fft,  test_output_fft, verbose=2)
+
+
+
 
 """ model = keras.Sequential([
     #keras.layers.Flatten(input_shape=(28, 28)),
